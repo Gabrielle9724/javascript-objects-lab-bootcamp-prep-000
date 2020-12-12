a9664b32ca1cc37e87aeeb2048237dc1202ceceb
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-12 21:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var obj = {}
 function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign ({}, obj, {[key] : value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj.prop2 = 2
+  return obj
+}
